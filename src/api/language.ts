46405fda7899329c6f4f6abce66cb874dbff0536
Kim Young Woo <85552569+hppYW@users.noreply.github.com>
2026-07-12 17:42:25 +0900

    fix: 학습 언어 전환 기능 수정

--- src/api/language.ts
+++ src/api/language.ts
@@ -1,5 +1,10 @@
 import client from './client';
 
 export const switchLanguage = async (language: string): Promise<void> => {
-  await client.post('/switch', { language });
+  await client.post('api/v1/switch', { language });
+};
+
+export const checkLanguage = async (lang: string): Promise<boolean> => {
+  const res = await client.post('api/v1/langcheck', null, { params: { lang } });
+  return res.data;
 };
